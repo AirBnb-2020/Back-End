30d14915467a7ac904deddfa96b0f4105346d137
gdhaliwal22 <47346292+gdhaliwal22@users.noreply.github.com>
2020-05-20 15:21:49 -0700

    Update restrictedAuthMiddleware.js

--- auth/restrictedAuthMiddleware.js
+++ auth/restrictedAuthMiddleware.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    const secret = process.env.JWT_SECRET || "willy wonka";
+    const secret = process.env.JWT_SECRET;
 
     jwt.verify(token, secret, (err, decodedToken) => {
       if (err) {
